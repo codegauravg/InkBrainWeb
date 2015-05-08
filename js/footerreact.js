6f11327b18b74890823b09f66b57fa08e3e902d9
codegauravg <codegauravg@gmail.com>
2015-05-08 21:02:08 +0530

    footer completed

--- js/footerreact.js
+++ js/footerreact.js
@@ -8,12 +8,14 @@ var footer = React.createClass({displayName: "footer",
                                                                             React.createElement("i",{className: 'fa fa-copyright'},null)," Licensed under MIT"),React.createElement("i",{className: 'fa fa-copyright'},null)," InkBrain 2014-2015"
                                                       ),
                                React.createElement("p",{className: 'navbar-text navbar-right'},
-                                                   "Development &amp; Maintained with ",
+                                                   "Development & Maintained with ",
                                                    React.createElement("i",{className: 'fa fa-heart'},null),
                                                    " on ", React.createElement("b",null,React.createElement("i",{className: 'fa fa-copyright'},null),React.createElement("a",{href: 'http://getbootstrap.com'}," Bootstrap "),
-                                                                               React.createElement("i",{className: 'fa fa-copyright'},null),React.createElement("a",{href: 'https://www.twitter.com'}," Twitter "))," API ",
+                                                                               React.createElement("i",{className: 'fa fa-copyright'},null),React.createElement("a",{href: 'https://www.twitter.com'}," Twitter ")," API "),
+                                                                              React.createElement("br",null,null),"& Rendered on: ",React.createElement("b",null,React.createElement("a",{href: 'https://facebook.github.io/react/'},React.createElement("i",{className: 'fa fa-facebook'},null)," React")),
+
                                                    React.createElement("br",null,null),"By: ",React.createElement("a",{href: 'https://github.com/codegauravg'}," codegauravg"),
-                                                   " &amp; ",React.createElement("a",{href: 'https://github.com/anuragkumarak95'}," anuragkumarak95")
+                                                   " & ",React.createElement("a",{href: 'https://github.com/anuragkumarak95'}," anuragkumarak95")
                                                   )
                                     )
                                 )
