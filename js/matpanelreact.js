9738eb02c2b0702e96a0a2150aed081fb874af81
anuragkumarak95 <anuragkumarak95@gmail.com>
2015-05-09 15:22:21 +0530

    matpanel react

---
+++ js/matpanelreact.js
@@ -0,0 +1,35 @@
+var matpanel = React.createClass({displayName: "matpanel",
+  render: function(){
+    return(
+      React.createElement("div",{className: 'col-xs-12 col-sm-12 col-md-12 col-lg-12'},
+       React.createElement("div",{className: 'box'},
+        React.createElement("div",{className: 'box-icon'},
+         React.createElement("span",{className: this.props.icon},null)
+        ),
+        React.createElement("div",{className: 'info'},
+         React.createElement("h4",{className: 'text-center'},this.props.head),
+         React.createElement("p",null,this.props.children),
+        React.createElement("a",{href: this.props.btnlink, className: 'btn'},this.props.btnlabel)
+        )
+       )
+      )
+    );
+
+  }
+
+});
+
+var mpanelgrp = React.createClass({displayName: "mpanelgrp",
+  render: function(){
+    return(
+      React.createElement("div",{className: 'row'},
+       React.createElement("div",{onChange: this.onChange ,className: this.props.colsize},
+       this.props.options.map(function(option){return(
+         React.createElement(matpanel,{icon: option.icon,head: option.head,btnlabel: option.btnlabel,btnlink: option.btnlink, key: option.head},option.content)
+         );
+       })
+       )
+      )
+    );
+  }
+});
